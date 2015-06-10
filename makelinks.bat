a6ae115f468db1c567d6c7050feafd350521622e
Christopher Heuer <cheuer@gmail.com>
2015-06-10 14:27:20 -0600

    Adding scripts to create links to the dotfiles

---
+++ makelinks.bat
@@ -0,0 +1,21 @@
+@echo off
+
+:: check for admin rights and try to elevate
+whoami /groups | findstr /b BUILTIN\Administrators | findstr /c:"Enabled group" > NUL && goto :run
+echo Running command as Administrator...
+elevate "makelinks.bat" && goto :end
+echo Failed to elevate. Try running this script as Administrator.
+pause
+goto :end
+
+:run
+@echo on
+mklink %USERPROFILE%\.bash_history %USERPROFILE%\Dropbox\bin\bash\.bash_history
+mklink %USERPROFILE%\.bash_profile %USERPROFILE%\.dotfiles\bash_profile
+mklink %USERPROFILE%\.bashrc %USERPROFILE%\.dotfiles\bashrc
+mklink %USERPROFILE%\.gitconfig %USERPROFILE%\.dotfiles\gitconfig
+mklink %USERPROFILE%\.vimrc %USERPROFILE%\.dotfiles\vimrc
+@echo off
+pause
+
+:end
---
+++ makelinks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -sv ~/.bash_profile ~/.dotfiles/bash_profile
+ln -sv ~/.bashrc ~/.dotfiles/bashrc
+ln -sv ~/.gitconfig ~/.dotfiles/gitconfig
+ln -sv ~/.vimrc ~/.dotfiles/vimrc
