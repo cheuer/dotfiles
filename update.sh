e1472f4a8e102082cec47e9f23614513bcbe4b85
Christopher Heuer <cheuer@gmail.com>
2015-11-19 15:55:36 -0700

    work when called from outside dotfiles dir

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+dotfiles_dir=~/.dotfiles
+
 # use git if available
-hash git 2>/dev/null && git pull && exit
+hash git 2>/dev/null && git -C $dotfiles_dir pull && exit
 
 # fallback to wget
 wget https://github.com/cheuer/dotfiles/archive/master.tar.gz
 tar -xzf master.tar.gz
-[[ ! -d ~/.dotfiles ]] && mkdir ~/.dotfiles
-cp dotfiles-master/* ~/.dotfiles/
+[[ ! -d $dotfiles_dir ]] && mkdir $dotfiles_dir
+cp dotfiles-master/* $dotfiles_dir/
 rm -rf master.tar.gz dotfiles-master
 echo dotfiles updated!
