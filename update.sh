6fed7a359628f026dccd7c5aa735e2dbd9709bcb
Christopher Heuer <cheuer@gmail.com>
2015-11-20 16:17:10 -0700

    split update into functions to hopefully help with self-update issue

--- update.sh
+++ update.sh
@@ -2,13 +2,22 @@
 
 dotfiles_dir=~/.dotfiles
 
-# use git if available
-hash git 2>/dev/null && git -C $dotfiles_dir pull && exit
+function git_update() {
+	git -C $dotfiles_dir pull && exit
+}
 
-# fallback to wget
-wget https://github.com/cheuer/dotfiles/archive/master.tar.gz
-tar -xzf master.tar.gz
-[[ ! -d $dotfiles_dir ]] && mkdir $dotfiles_dir
-cp --remove-destination dotfiles-master/* $dotfiles_dir/
-rm -rf master.tar.gz dotfiles-master
+function wget_update() {
+	wget https://github.com/cheuer/dotfiles/archive/master.tar.gz
+	tar -xzf master.tar.gz
+	[[ ! -d $dotfiles_dir ]] && mkdir $dotfiles_dir
+	cp dotfiles-master/* $dotfiles_dir/
+	rm -rf master.tar.gz dotfiles-master
+}
+
+if hash git 2>/dev/null; then
+	git_update
+else
+	wget_update
+fi
+
 echo Dotfiles updated!
