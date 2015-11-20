6dc52c3589e98ad6709535dfe52e93b527648b2c
Christopher Heuer <cheuer@gmail.com>
2015-11-20 15:59:23 -0700

    remove destination when copying to avoid overwriting running script

--- update.sh
+++ update.sh
@@ -9,6 +9,6 @@ hash git 2>/dev/null && git -C $dotfiles_dir pull && exit
 wget https://github.com/cheuer/dotfiles/archive/master.tar.gz
 tar -xzf master.tar.gz
 [[ ! -d $dotfiles_dir ]] && mkdir $dotfiles_dir
-cp dotfiles-master/* $dotfiles_dir/
+cp --remove-destination dotfiles-master/* $dotfiles_dir/
 rm -rf master.tar.gz dotfiles-master
-echo dotfiles updated!
+echo Dotfiles updated!
