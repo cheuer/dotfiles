9e82dea8e46f13d4ee40707216b037f36f29adab
Christopher Heuer <cheuer@gmail.com>
2016-08-23 10:51:35 -0600

    Fix update.sh getting modified while updating itself

--- update.sh
+++ update.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
+{
+	dotfiles_dir=~/.dotfiles
 
-dotfiles_dir=~/.dotfiles
+	function git_update() {
+		git -C $dotfiles_dir pull && exit
+	}
 
-function git_update() {
-	git -C $dotfiles_dir pull && exit
+	function wget_update() {
+		wget https://github.com/cheuer/dotfiles/archive/master.tar.gz 2>/dev/null
+		tar -xzf master.tar.gz
+		[[ ! -d $dotfiles_dir ]] && mkdir $dotfiles_dir
+		cp dotfiles-master/* $dotfiles_dir/
+		rm -rf master.tar.gz dotfiles-master
+	}
+
+	if ! hash git 2>/dev/null || [[ $(git --version | sed "s/git version //") < "1.9" ]]; then
+		wget_update
+	else
+		git_update
+	fi
+
+	echo Dotfiles updated!
+
+	exit
 }
-
-function wget_update() {
-	wget https://github.com/cheuer/dotfiles/archive/master.tar.gz 2>/dev/null
-	tar -xzf master.tar.gz
-	[[ ! -d $dotfiles_dir ]] && mkdir $dotfiles_dir
-	cp dotfiles-master/* $dotfiles_dir/
-	rm -rf master.tar.gz dotfiles-master
-}
-
-if ! hash git 2>/dev/null || [[ $(git --version | sed "s/git version //") < "1.9" ]]; then
-	wget_update
-else
-	git_update
-fi
-
-echo Dotfiles updated!
