5489a655948dcbabca9debe9ed595a83e3b96929
Chris Heuer <cheuer@gmail.com>
2024-07-10 18:56:06 -0600

    make the dir

--- makelinks.bat
+++ makelinks.bat
@@ -14,6 +14,7 @@ mklink %USERPROFILE%\.bash_profile %USERPROFILE%\.dotfiles\bash_profile
 mklink %USERPROFILE%\.bashrc %USERPROFILE%\.dotfiles\bashrc
 mklink %USERPROFILE%\.gitconfig %USERPROFILE%\.dotfiles\gitconfig
 mklink %USERPROFILE%\.vimrc %USERPROFILE%\.dotfiles\vimrc
+mkdir %USERPROFILE%\Documents\Powershell
 mklink %USERPROFILE%\Documents\Powershell\Microsoft.PowerShell_profile.ps1 %USERPROFILE%\.dotfiles\Microsoft.PowerShell_profile.ps1
 @echo off
 pause
