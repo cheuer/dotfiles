ab38929160d1cb8d2f0315185b6d5bcf9a0be46b
Chris Heuer <cheuer@gmail.com>
2024-08-26 12:21:48 -0600

    Add grep alias and a few things

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,20 @@
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 set-alias npp "C:\Program Files\Notepad++\notepad++.exe"
+set-alias grep select-string
 
 function ga {git add $args}
 function gd {git.exe diff $args}
 function gds {git.exe diff --staged $args}
 function gf {git.exe fetch $args}
-function gh {git.exe hist $args}
-function gs {git.exe status -uno $args}
+function gg {git.exe hist $args}
+function gs {git.exe status $args}
 
 function manage{
 	poetry run python manage.py $args
 }
 function testall{
-	poetry run pytest --create-db --functional --unit --integration --cov --cov-report=xml --disable-warnings $args
-	poetry run pytest --reuse-db --functional --unit --integration --cov --cov-report=xml --disable-warnings $args
+	poetry run pytest --create-db --functional --unit --integration --disable-warnings $args && poetry run pytest --reuse-db --functional --unit --integration --cov --cov-report=xml --disable-warnings $args
 }
 function retest{
 	poetry run pytest --reuse-db --functional --unit --integration --cov --cov-report=xml --disable-warnings --last-failed
@@ -24,3 +24,5 @@ oh-my-posh init pwsh --config ~/.dotfiles/ohmyposhv3.json | Invoke-Expression
 
 # disable venv prompt
 $env:VIRTUAL_ENV_DISABLE_PROMPT=1
+
+Import-Module posh-git
