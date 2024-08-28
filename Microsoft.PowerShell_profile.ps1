13f19b49468f56b2df71d946a69826f8e1f1b9f9
Chris Heuer <cheuer@gmail.com>
2024-08-28 17:01:24 -0600

    Add shortcuts and useful things

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,10 @@
+# bash-like shortcuts
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineKeyHandler -Key Alt+b -Function ShellBackwardWord
+Set-PSReadLineKeyHandler -Key Alt+f -Function ShellForwardWord
+Set-PSReadLineKeyHandler -Key Alt+B -Function SelectShellBackwardWord
+Set-PSReadLineKeyHandler -Key Alt+F -Function SelectShellForwardWord
+Set-PSReadLineKeyHandler -Key Ctrl+k -Function ForwardDeleteInput
 
 set-alias npp "C:\Program Files\Notepad++\notepad++.exe"
 set-alias grep select-string
@@ -17,7 +23,7 @@ function testall{
 	poetry run pytest --create-db --functional --unit --integration --disable-warnings $args && poetry run pytest --reuse-db --functional --unit --integration --cov --cov-report=xml --disable-warnings $args
 }
 function retest{
-	poetry run pytest --reuse-db --functional --unit --integration --cov --cov-report=xml --disable-warnings --last-failed
+	poetry run pytest --reuse-db --functional --unit --integration --cov --cov-report=xml --disable-warnings --last-failed $args
 }
 
 oh-my-posh init pwsh --config ~/.dotfiles/ohmyposhv3.json | Invoke-Expression
@@ -26,3 +32,49 @@ oh-my-posh init pwsh --config ~/.dotfiles/ohmyposhv3.json | Invoke-Expression
 $env:VIRTUAL_ENV_DISABLE_PROMPT=1
 
 Import-Module posh-git
+Import-Module CompletionPredictor
+
+# fix up/down in python shells https://github.com/microsoft/terminal/issues/2558#issuecomment-2069286909
+py -c "import ctypes; ctypes.windll.kernel32.SetConsoleHistoryInfo(b'\x10\x00\x00\x00\x00\x02\x00\x00 \x00\x00\x00\x01\x00\x00\x00')"
+
+# Don't add to history if short or common https://stackoverflow.com/a/52818502
+$addToHistoryHandler = {
+    Param([string]$line)
+    if ($line.Length -le 3) {
+        return $false
+    }
+    if (@("exit","dir","ls","pwd","cd ..").Contains($line.ToLowerInvariant())) {
+        return $false
+    }
+    return $true
+}
+Set-PSReadlineOption -AddToHistoryHandler $addToHistoryHandler
+
+
+# Remove history items with shift+del https://stackoverflow.com/a/77959050
+Set-PSReadLineKeyHandler -Chord Shift+Delete -Description "Search through the history and remove lines starting with entered text" -ScriptBlock {
+    $in = ''
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref] $in, [ref] $null)
+
+    if ([string]::IsNullOrWhiteSpace($in)) {
+        return
+    }
+
+    $historyPath = (Get-PSReadLineOption).HistorySavePath
+
+    # only way to "refresh" the history in the current session is to clear it
+    [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+    $content = [System.IO.File]::ReadAllLines($historyPath)
+    Clear-Content $historyPath
+
+    foreach ($line in $content) {
+        if ($line.StartsWith($in, [System.StringComparison]::InvariantCultureIgnoreCase)) {
+            continue
+        }
+
+        # and re-add it (excluding the line to remove)
+        [Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($line)
+    }
+
+    [Microsoft.PowerShell.PSConsoleReadLine]::DeleteLine()
+}
